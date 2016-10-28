5977f3d1cf49ab1bf438f9730b5df7491f417d0b
mismith0227 <doraepon2216@gmail.com>
2016-10-29 07:58:45 +0900

    edit code

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 var postcss = require('postcss');
-var chara = require('./ghost.json');
+var ghost = require('./ghosts.json');
 
 module.exports = postcss.plugin('postcss-myplugin', function () {
     return function (css) {
-        css.walkDecls('まほう', function (decl) {
-            var ghosts = postcss.list.space(decl.value);
+        css.walkDecls('おばけ', function (decl) {
+            var ghostvalue = postcss.list.space(decl.value);
 
             decl.cloneBefore({ prop: 'background-repeat', value: 'no-repeat' });
             decl.cloneBefore({ prop: 'background-size', value: 'cover' });
             decl.cloneBefore({ prop: 'transform-origin', value: '0 0' });
+            decl.cloneBefore({ prop: 'width',  value: '400px' });
+            decl.cloneBefore({ prop: 'height', value: '400px' });
 
-            if ( ghosts == 'ほのお' ) {
-              decl.cloneBefore({ prop: 'width',  value: '400px' });
-              decl.cloneBefore({ prop: 'height', value: '400px' });
-              decl.cloneBefore({ prop: 'background-image', value: 'url('+ chara.fire +')' });
-
-            } else if (ghosts == 'つえ') {
-              decl.cloneBefore({ prop: 'width',  value: '400px' });
-              decl.cloneBefore({ prop: 'height', value: '400px' });
-              decl.cloneBefore({ prop: 'background-image', value: 'url('+ chara.stick +')' });
+            if ( ghostvalue == 'おおかみおとこ' ) {
+              decl.cloneBefore({ prop: 'background-image', value: 'url('+ ghost.wolfman +')' });
+            } else if (ghostvalue == 'おばけ') {
+              decl.cloneBefore({ prop: 'background-image', value: 'url('+ ghost.ghost +')' });
+            } else if (ghostvalue == 'かぼちゃおばけ') {
+              decl.cloneBefore({ prop: 'background-image', value: 'url('+ ghost.pumpkin +')' });
+            } else if (ghostvalue == 'こうもり') {
+              decl.cloneBefore({ prop: 'background-image', value: 'url('+ ghost.bats +')' });
+            } else if (ghostvalue == 'まじょ') {
+              decl.cloneBefore({ prop: 'background-image', value: 'url('+ ghost.witch +')' });
             }
 
             decl.remove();
